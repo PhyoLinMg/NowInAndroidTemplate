dee6b105d452fdbd12364493834b15e026bca16e
Lin Maung <gangsterplm@gmail.com>
2023-06-18 14:53:01 +0800

    first step completed

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+
+        // Android Build Server
+        maven { url = uri("../nowinandroid-prebuilts/m2repository") }
+    }
+
+    dependencies{
+        classpath(libs.hilt.gradlePlugin)
+    }
+}
+
+// Lists all plugins used throughout the project without applying them.
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.firebase.perf)
+    alias(libs.plugins.gms)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.secrets)
+}
